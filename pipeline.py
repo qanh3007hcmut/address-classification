3e39dfc4fa9d2b7a961a65342ad349b305ab942c
Nguyễn Quốc Anh <168700063+qanh3007hcmut@users.noreply.github.com>
2025-10-07 11:07:44 +0700

    hotfix pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,7 @@
 from utils.data import get_data, get_prefix_dict
 from utils.trie import build_all_automaton, trie_pipeline
 from utils.fuzz import fuzz_pipeline
-from utils.input import preprocess_input, select_candidate
+from utils.input import preprocess_input, select_candidate_by_order_administrative
 
 DATA = get_data()
 PREFIX = get_prefix_dict()
@@ -10,6 +10,5 @@ AUTOMATON = build_all_automaton(DATA, PREFIX)
 def process(input : str):
     result = trie_pipeline(input, AUTOMATON)
     result = fuzz_pipeline(input, result)
-    if len(result) > 1:    
-        result = select_candidate(result, preprocess_input(input).split(","))
+    result = select_candidate_by_order_administrative(result, input)
     return result[0][0]
